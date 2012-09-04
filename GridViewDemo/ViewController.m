222c89fcf8653766abd2d5505e0481048dae43f8
Eric Yang <vovodev@gmail.com>
2012-09-04 09:41:58 +0800

    Add a label

--- GridViewDemo/ViewController.m
+++ GridViewDemo/ViewController.m
@@ -17,12 +17,16 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    UILabel *label = [[UILabel alloc] initWithFrame:CGRectMake(20.0f, 20.0f, 200.0f, 30.0f)];
+    label.text = @"Text";
+    [self.view addSubview:label];
 	// Do any additional setup after loading the view, typically from a nib.
 }
 
 - (void)viewDidUnload
 {
     [super viewDidUnload];
+    
     // Release any retained subviews of the main view.
 }
 
